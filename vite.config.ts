fe7879cba79c46c79c482286ad4bd0c8b091194c
mikigoalie <mikigoalie@seznam.cz>
2023-10-24 05:50:20 +0200

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://saram7619.github.io/deploying-vite-project-example/",
+});
